cc7dea961cad09a47ed71fbdede286d774acce7c
Daniel Ji <daniel.ji0923@gmail.com>
2023-11-04 13:16:23 -0700

    updated repository

---
+++ answers.js
@@ -0,0 +1,65 @@
+////////////////////////////////
+// Easy Going
+////////////////////////////////
+for (let i=1; i<21; i++){
+    if (i=20){
+        console.log("Easy Going answered");
+    }
+}
+
+////////////////////////////////
+// Get Even
+////////////////////////////////
+for (let i=0; i<201; i+=2){
+    if (i === 200){
+        console.log("Get Even answered");
+    }
+}
+
+////////////////////////////////
+// Fizz Buzz                            RECHECK THIS SOLUTION
+////////////////////////////////
+/*
+for (let i=1; i<101; i++){
+    if (i % 3 === 0){
+        console.log("Fizz")
+    }else if(i % 5 === 0){
+        console.log("Buzz")
+        }else if(i % 3 || 5 === 0){
+            console.log("FizzBuzz")
+    }
+}
+*/
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+
+////////////////////////////////
+// Yell at the Ninja Turtles
+////////////////////////////////
+
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
+
+
+////////////////////////////////
+// Where is Waldo
+////////////////////////////////
+
+
+////////////////////////////////
+//  Excited Kitten
+////////////////////////////////
+
+
+////////////////////////////////
+//  Find the Median
+////////////////////////////////
